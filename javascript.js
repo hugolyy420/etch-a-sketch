d5c2b8d6bac7067f09143de88d647cb576b81a86
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2023-08-02 23:47:12 -0400

    Fix the lagging issue when changing number of grids

--- javascript.js
+++ javascript.js
@@ -1,28 +1,43 @@
 const gridContainer = document.querySelector(".container");
 let interval = null;
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.textContent = slider.value + " x " + slider.value ;
 
 function makeGrids (rowNumber) {
     let total = rowNumber * rowNumber + 1;
+    const containerWidth = document.querySelector(".container").clientHeight;
 
 for (let i = 1; i < total; i++) {
-    let h = rowNumber ;
     const smallGrid = document.createElement("div");
-    const containerWidth = document.querySelector(".container").clientHeight;
     smallGrid.style.width = containerWidth / rowNumber + "px";
     smallGrid.style.height = containerWidth / rowNumber + "px";
     smallGrid.classList.add("small-grid");
     gridContainer.appendChild(smallGrid);
 }}
 
-makeGrids (40);
-
+makeGrids(16);
 
 function changeColor (e) {
         e.target.classList.add("change-color");
     }
 
-let grids = document.querySelectorAll(".small-grid");   
+let grids = document.querySelectorAll(".small-grid");     
 grids.forEach((grid) => { 
     grid.addEventListener("mouseover", changeColor)}
 );
 
+slider.oninput = function() {
+    output.textContent = this.value + " x " + this.value;
+}
+
+slider.addEventListener('mouseup', function () {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    makeGrids(slider.value);
+    let grids = document.querySelectorAll(".small-grid");     
+    grids.forEach((grid) => { 
+        grid.addEventListener("mouseover", changeColor)}
+);
+})
