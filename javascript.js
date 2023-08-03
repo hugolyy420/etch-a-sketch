d2c97481a50d2b3fcb140164fb528f0a5f805e10
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2023-08-02 22:19:23 -0400

    Change the function and allow users to customize the number or grids

--- javascript.js
+++ javascript.js
@@ -1,25 +1,28 @@
 const gridContainer = document.querySelector(".container");
+let interval = null;
 
 function makeGrids (rowNumber) {
     let total = rowNumber * rowNumber + 1;
 
 for (let i = 1; i < total; i++) {
+    let h = rowNumber ;
     const smallGrid = document.createElement("div");
     const containerWidth = document.querySelector(".container").clientHeight;
-    // smallGrid.style.width = "containerWidth / rowNumber";
-    // smallGrid.style.height = "containerWidth / rowNumber";
-    // smallGrid.style.border = "0.5px solid black";
+    smallGrid.style.width = containerWidth / rowNumber + "px";
+    smallGrid.style.height = containerWidth / rowNumber + "px";
     smallGrid.classList.add("small-grid");
     gridContainer.appendChild(smallGrid);
 }}
 
-makeGrids (20);
+makeGrids (40);
+
 
 function changeColor (e) {
-        e.currentTarget.classList.add("change-color");
+        e.target.classList.add("change-color");
     }
 
 let grids = document.querySelectorAll(".small-grid");   
 grids.forEach((grid) => { 
     grid.addEventListener("mouseover", changeColor)}
 );
+
